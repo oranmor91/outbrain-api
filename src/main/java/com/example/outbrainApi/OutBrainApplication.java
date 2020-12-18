6f49eb28f7075bf365ad091d54bdc384ee82bbe7
Oran Mor <oran@coralogix.com>
2020-12-18 17:41:20 +0200

    oran-test

--- src/main/java/com/example/outbrainApi/OutBrainApplication.java
+++ src/main/java/com/example/outbrainApi/OutBrainApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OutBrainApplication {
-
+	//oran-test
 	public static void main(String[] args) {
 		SpringApplication.run(OutBrainApplication.class, args);
 	}
